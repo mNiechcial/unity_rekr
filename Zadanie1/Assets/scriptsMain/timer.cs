8a855266a96803883e971c973e7c2b95a02033a7
mNiechcial <36309995+mNiechcial@users.noreply.github.com>
2018-02-09 20:46:10 +0100

    first update
    babysteps, interface

---
+++ Zadanie1/Assets/scriptsMain/timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+	public float endTime = 10.0f;
+	public Text timerText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (endTime > 0.0f) 
+		{
+			endTime -= Time.deltaTime;
+			timerText.text = endTime.ToString("0.0");
+		}
+
+		if (endTime <= 0.0f) 
+		{
+			timerText.text = ("KONIEC CZASU");	
+		}
+	}
+}
